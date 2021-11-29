e39c4204a1211b13705208a958fac56ed3db47ee
jhy950210 <jhyy1995210@gmail.com>
2021-11-29 21:40:41 +0900

    test : startQuerydsl

--- src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -1,9 +1,9 @@
 package com.study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.study.querydsl.entity.Hello;
-import com.study.querydsl.entity.QHello;
+import com.study.querydsl.entity.*;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,15 +12,35 @@ import org.springframework.test.annotation.Commit;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import static com.study.querydsl.entity.QMember.*;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@Commit // rollback 하지 않음
+//@Commit // rollback 하지 않음
 class QuerydslApplicationTests {
 
 	@Autowired
 	EntityManager em;
+
+	JPAQueryFactory queryFactory;
+
+	@BeforeEach
+	public void before() {
+		queryFactory = new JPAQueryFactory(em);
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member1);
+		em.persist(member2);
+		em.persist(member3);
+		em.persist(member4);
+	}
 
 	@Test
 	void contextLoads() {
@@ -37,6 +57,18 @@ class QuerydslApplicationTests {
 
 		assertThat(result).isEqualTo(hello);
 	}
+
+	@Test
+	public void startQuerydsl(){
+		Member findMember = queryFactory
+				.select(member)
+				.from(member)
+				.where(member.name.eq("member1"))
+				.fetchOne();
+
+		assertThat(findMember.getName()).isEqualTo("member1");
+	}
+
 
 
 }
